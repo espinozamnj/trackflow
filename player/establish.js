f084fe90e08fa630a611a2ed085b5e0d6fedde1e
espinozamnj <1052179@senati.pe>
2023-07-15 16:06:43 -0500

    all

--- player/establish.js
+++ player/establish.js
@@ -2,18 +2,18 @@
 // let redirList = 'all'
 
 (function() {
-    if (location.hash == '' || location.hash == '#') {
-        if (typeof(redirList) == 'undefined') {
-            location.hash = 'public'
-        } else {
-            location.hash = redirList
-        }
+  if (location.hash == '' || location.hash == '#') {
+    if (typeof(redirList) == 'undefined') {
+      location.hash = 'public'
+    } else {
+      location.hash = redirList
     }
-    let selectDir = location.hash.replace('#', '')
-    let baseScript = document.createElement('script')
-    baseScript.src = './dmx/' + selectDir + '/list-data-music.js'
-    $('.js').appendChild(baseScript)
-    baseScript.onerror = function() {
-        $('.player').innerHTML = '<div class="red">LISTA NO ENCONTRADA</div>'
-    }
+  }
+  let selectDir = location.hash.replace('#', '')
+  let baseScript = document.createElement('script')
+  baseScript.src = './dmx/' + selectDir + '/list-data-music.js'
+  $('.js').appendChild(baseScript)
+  baseScript.onerror = function() {
+    $('.player').innerHTML = '<div class="red">LISTA NO ENCONTRADA</div>'
+  }
 })()
